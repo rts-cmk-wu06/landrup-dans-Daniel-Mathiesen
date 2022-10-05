de609b42bfa2a01a65a6615575f7687606bb65c3
Dusty707 <77662695+Daniel-Mathiesen@users.noreply.github.com>
2022-10-05 11:38:39 +0200

    feat: :sparkles: Add search page
    Add search page UI
    
    no bugs

--- app/src/pages/Search.js
+++ app/src/pages/Search.js
@@ -1,6 +1,21 @@
+// Components
+import Menu from "../components/Menu";
+
+// Icons
+import { FiSearch } from "react-icons/fi";
+
 const SearchPage = () => {
     return (  
-        <>Søg</>
+        <div className="bg-landrupPurple h-screen w-screen pt-24">
+            <h1 className="text-white font-lg text-4xl px-8 pt-8 fixed z-50 top-0 bg-landrupPurple w-full">Søg</h1>
+            <div className="px-8">
+                <input className="bg-slate-400 p-2 pr-10 w-full opacity-60 outline-none" type="text" placeholder="" />
+                <FiSearch className="text-white w-6 h-6 relative left-80 -translate-y-8"/>
+            </div>
+            <div>
+                <Menu/>
+            </div>
+        </div>
     );
 }
  
