3a13d7c50f86ff0db2539f85831406e59a31fd4a
Dusty707 <77662695+Daniel-Mathiesen@users.noreply.github.com>
2022-10-05 12:54:23 +0200

    feat: :lipstick: Add calender page
    Add calender page ui and calender card component
    
    no bugs

---
+++ app/src/components/CalenderCard.js
@@ -0,0 +1,12 @@
+const CalenderCard = () => {
+    return ( 
+        <div className="CalenderCard_wrapper flex justify-center px-10">
+            <div className="bg-white w-full flex flex-col items-start rounded-lg p-5">
+                <h1 className="text-3xl">ActivityName</h1>
+                <h2 className="text-lg">Date Time</h2>
+            </div>
+        </div>
+    );
+}
+ 
+export default CalenderCard;
